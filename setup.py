3292224e8c5fda07b27ea2d7a81f93a1b8c63d06
unknown <ger-adv@yandex.ru>
2024-07-22 20:12:31 +0300

    Доделал Readme, проверил работоспособность библиотеки

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="mindbox_area_calculator",
     version="0.1",
-    packages=find_packages(), 
+    author="Gerich02",
+    description="Библиотека для вычисления площадей круга и треугольника.",
+    url="https://github.com/gerich02/Mindbox_test/tree/main/mindbox_area_calculator",
+    readme="README.md",
+    packages=find_packages(),
     install_requires=[],
+    python_requires=">=3.9",
 )
